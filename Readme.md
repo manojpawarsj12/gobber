71567afebbbc52cf99c1e4e5757f444345f5306b
Manoj Pawar SJ <manoj.pawar@digantara.co.in>
2025-03-04 18:33:11 +0530

    refactor: improve package installation logic and error handling in installer

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Gobber
+
+Gobber is a package manager for Go, designed to simplify the process of managing dependencies for your Go projects.
+
+## Installation
+
+To install Gobber, clone the repository and build the project:
+
+```sh
+git clone https://github.com/manojpawarsj12/gobber.git
+cd gobber
+go build -o gobber cmd/main.go
+```
+
+## Usage
+
+To use Gobber, you can run the install command to install packages. For example, to install the latest version of the express package, you can use the following command:
+
+```sh
+./gobber install -p express@latest
+```
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
--- internal/installer.go
+++ internal/installer.go
@@ -1,122 +1,136 @@
 package internal
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"sync"
+    "fmt"
+    "log"
+    "os"
+    "path/filepath"
+    "sync"
 )
 
 func checkIsInstalled(name string, version string, InstalledVersionsMutex *map[string]string, mut *sync.Mutex) bool {
-	mut.Lock()
-	defer mut.Unlock()
-	packageData := (*InstalledVersionsMutex)[name]
-	return packageData == version
+    mut.Lock()
+    defer mut.Unlock()
+    packageData := (*InstalledVersionsMutex)[name]
+    return packageData == version
 }
 
 func ancestorInstalled(name string, projectDir string, InstalledVersionsMutex *map[string]string, mut *sync.Mutex) bool {
-	dir := projectDir
-	for {
-		mut.Lock()
-		_, isInstalled := (*InstalledVersionsMutex)[name]
-		mut.Unlock()
-		if isInstalled {
-			return true
-		}
-		parentDir := filepath.Dir(dir)
-		if parentDir == dir {
-			break
-		}
-		dir = parentDir
-	}
-	return false
+    dir := projectDir
+    for {
+        mut.Lock()
+        _, isInstalled := (*InstalledVersionsMutex)[name]
+        mut.Unlock()
+        if isInstalled {
+            return true
+        }
+        parentDir := filepath.Dir(dir)
+        if parentDir == dir {
+            break
+        }
+        dir = parentDir
+    }
+    return false
 }
 
 func InstallPackageRecursive(packageData PackageData, InstalledVersionsMutex *map[string]string, projectDir string, mut *sync.Mutex) error {
-	if checkIsInstalled(packageData.Name, packageData.Version, InstalledVersionsMutex, mut) {
-		return nil
-	}
-	log.Println("installing !!!!!", packageData.Name, packageData.Version)
+    if checkIsInstalled(packageData.Name, packageData.Version, InstalledVersionsMutex, mut) {
+        return nil
+    }
+    log.Println("installing !!!!!", packageData.Name, packageData.Version)
 
-	if ancestorInstalled(packageData.Name, projectDir, InstalledVersionsMutex, mut) {
-		return nil
-	}
+    if ancestorInstalled(packageData.Name, projectDir, InstalledVersionsMutex, mut) {
+        return nil
+    }
 
-	// Create a directory for the package in the node_modules folder
-	packageDestDir := filepath.Join(projectDir, "node_modules", packageData.Name)
-	if err := os.MkdirAll(packageDestDir, 0755); err != nil {
-		return fmt.Errorf("mkdir failed: %v", err)
-	}
+    // Create a directory for the package in the node_modules folder
+    packageDestDir := filepath.Join(projectDir, "node_modules", packageData.Name)
+    if err := os.MkdirAll(packageDestDir, 0755); err != nil {
+        return fmt.Errorf("mkdir failed: %v", err)
+    }
 
-	extractionDone := make(chan struct{})
+    extractionDone := make(chan error)
 
-	go func() {
-		defer close(extractionDone)
+    go func() {
+        defer close(extractionDone)
 
-		// Extract the tarball to the package directory
-		tarballUrl := packageData.Dist.Tarball
-		if err := ExtractTar(&tarballUrl, &packageDestDir); err != nil {
-		}
+        // Extract the tarball to the package directory
+        tarballUrl := packageData.Dist.Tarball
+        if err := ExtractTar(&tarballUrl, &packageDestDir); err != nil {
+            extractionDone <- err
+            return
+        }
 
-		mut.Lock()
-		(*InstalledVersionsMutex)[packageData.Name] = packageData.Version
-		mut.Unlock()
-	}()
-	done := make(chan bool)
-	deps := packageData.Dependencies
+        mut.Lock()
+        (*InstalledVersionsMutex)[packageData.Name] = packageData.Version
+        mut.Unlock()
+        extractionDone <- nil
+    }()
 
-	for name, version := range deps {
-		comparator, err := v.parseSemanticVersion(version)
-		if err != nil {
-			return fmt.Errorf("error parsing semantic version: %v", err)
-		}
-		go func(name, version string) {
-			packageDetails := PackageDetails{Name: name, Comparator: comparator}
-			depPackageData, err := GetPackageData(&packageDetails)
-			if err != nil {
-				done <- false
-				return
-			}
-			// Install dependencies recursively inside the packageDestDir
-			if err := InstallPackageRecursive(depPackageData, InstalledVersionsMutex, packageDestDir, mut); err != nil {
-				done <- false
-				return
-			}
-			done <- true
-		}(name, version)
-	}
+    deps := packageData.Dependencies
+    depErrChan := make(chan error, len(deps))
 
-	for range deps {
-		// Wait for InstallPackageRecursive to complete
-		if !<-done {
-			return fmt.Errorf("error installing dependencies")
-		}
-	}
+    var wg sync.WaitGroup
+    for name, version := range deps {
+        wg.Add(1)
+        go func(name, version string) {
+            defer wg.Done()
+            comparator, err := v.parseSemanticVersion(version)
+            if err != nil {
+                depErrChan <- fmt.Errorf("error parsing semantic version: %v", err)
+                return
+            }
+            packageDetails := PackageDetails{Name: name, Comparator: comparator}
+            depPackageData, err := GetPackageData(&packageDetails)
+            if err != nil {
+                depErrChan <- err
+                return
+            }
+            // Install dependencies recursively inside the packageDestDir
+            if err := InstallPackageRecursive(depPackageData, InstalledVersionsMutex, packageDestDir, mut); err != nil {
+                depErrChan <- err
+                return
+            }
+            depErrChan <- nil
+        }(name, version)
+    }
 
-	// Wait for tarball extraction to complete
-	<-extractionDone
-	return nil
+    go func() {
+        wg.Wait()
+        close(depErrChan)
+    }()
+
+    for err := range depErrChan {
+        if err != nil {
+            return fmt.Errorf("error installing dependencies: %v", err)
+        }
+    }
+
+    // Wait for tarball extraction to complete
+    if err := <-extractionDone; err != nil {
+        return fmt.Errorf("error extracting tarball: %v", err)
+    }
+    return nil
 }
 
 func Execute(packageDetails *PackageDetails, mut *sync.Mutex, InstalledVersionsMutex *map[string]string, projectDir string) error {
-	done := make(chan bool)
+    done := make(chan error)
 
-	go func() {
-		packageData, err := GetPackageData(packageDetails)
-		if err != nil {
-			done <- false
-			return
-		}
-		if err := InstallPackageRecursive(packageData, InstalledVersionsMutex, projectDir, mut); err != nil {
-			done <- false
-			return
-		}
-		done <- true
-	}()
+    go func() {
+        packageData, err := GetPackageData(packageDetails)
+        if err != nil {
+            done <- err
+            return
+        }
+        if err := InstallPackageRecursive(packageData, InstalledVersionsMutex, projectDir, mut); err != nil {
+            done <- err
+            return
+        }
+        done <- nil
+    }()
 
-	if !<-done {
-		return fmt.Errorf("error executing package installation")
-	}
-	return nil
+    if err := <-done; err != nil {
+        return fmt.Errorf("error executing package installation: %v", err)
+    }
+    return nil
 }
